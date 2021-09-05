44e321dd50740f81f5c6ea20cf5ff8c772d3d8b1
Cataluna <catalunalilith2680@gmail.com>
2021-09-05 13:40:55 -0400

    innitial commit of old code associated with MIT OCW's 6.0001 coursework

--- 6.0001 PS0/exercise0.py
+++ 6.0001 PS0/exercise0.py
@@ -7,7 +7,7 @@ Created on Sun Oct 11 18:18:02 2020
 
 import numpy
 
-x=int(input("enter number x: "))
-y=int(input("enter number y: "))
+x = int(input("enter number x: "))
+y = int(input("enter number y: "))
 print("x**y=", x**y)
-print("log(x)=", numpy.log2(x) )
+print("log(x)=", numpy.log2(x))
