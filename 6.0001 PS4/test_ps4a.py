98995e4ee96d4a8d954cd4809efd0d8734c67ca8
Cataluna <catalunalilith2680@gmail.com>
2021-10-17 12:17:12 -0400

    unittest skeleton outlined

--- 6.0001 PS4/test_ps4a.py
+++ 6.0001 PS4/test_ps4a.py
@@ -8,5 +8,23 @@ Created on Fri Oct 15 19:12:41 2021
 import unittest
 import ps4a.py
 
-# TODO outline unittest skeleton
+
+class Testps4a(unittest.TestCase):
+
+    def test_get_permutations():
+        pass
+        # sequence len=0
+        # sequen len=1
+        # sequence len=3
+        # numbers
+        # letters
+        # space
+        # special characters
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+# TODO plan tests
+# TODO plan values for tests
 # TODO write unittests
